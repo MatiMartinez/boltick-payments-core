c441d02960a41c96ecdeb81b9e3f7f529fc20500
Matias Martinez <matias.martinez@farmaloop.cl>
2024-06-04 08:19:46 -0400

    informacion de email extra

--- src/services/notification/notification.service.ts
+++ src/services/notification/notification.service.ts
@@ -3,7 +3,7 @@ import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
 import { PaymentEntity } from '../../entities/payment.entity';
 
 export const notifyFreeTickets = async (payload: PaymentEntity) => {
-  const { user } = payload;
+  const { event, items, user } = payload;
 
   const sesClient = new SESClient({ region: 'us-east-1' });
 
@@ -19,28 +19,34 @@ export const notifyFreeTickets = async (payload: PaymentEntity) => {
             <td align="center" style="border: none; padding: 0">
               <div style="max-width: 500px; text-align: start">
                 <div style="padding-block: 24px; padding-left: 16px">
-                  <img src="https://boltick-public-assets.s3.amazonaws.com/boltick.svg" />
+                  <img src="https://boltick-public-assets.s3.amazonaws.com/boltick-negro.png" style="width: 150px" />
                 </div>
 
                 <br />
                 
                 <div style="padding-left: 16px">
-                  <h3>¡Gracias por unirte a ${payload.event} junto a Boltick!</h3>
+                  <h2>¡Gracias por unirte a ${event} junto a Boltick!</h2>
                 </div>
                 
                 <div style="padding-left: 16px">
-                  <p>¡Hola! <br />
-                  Hemos recibido correctamente tu solicitud de entradas. <br />
-                  Te informamos que el día 13 de junio recibirás las entradas en este mismo correo electrónico.</p>
-                  <p>¡Muchas gracias por tu participación!</p>
+                  <p style="font-size: 16px">¡Hola! Hemos recibido correctamente tu solicitud de entradas. Te informamos que el día 13 de junio recibirás las entradas en este mismo correo electrónico.</p>
+                  <p style="font-size: 16px">¡Muchas gracias por tu participación!</p>
                 </div>
 
                 <div style="padding-left: 16px">
-                  <p style="font-weight: 700">El equipo de Boltick</p>
+                  <div style="padding: 16px; border-radius: 4px; border: 1px solid #cecece">
+                      <p style="margin: 0; font-size: 20px; font-weight: 600;">${items[0].quantity} ${items[0].title}</p>
+                      <p style="margin: 0; font-size: 14px;">Centro de Congreso Francisco, Parque Agnesi, San Martín, Mendoza.</p>
+                      <a href="https://maps.app.goo.gl/ZwkeRuEQ56XoFSHo8" target="_blank" style="margin: 0; font-size: 16px">Ver ubicación</a>
+                  </div>
+                </div>
+
+                <br />
+
+                <div style="padding-left: 16px">
+                  <p style="font-size: 16px; font-weight: 700">El equipo de Boltick</p>
                 </div>        
     
-                <br />
-
                 <div style="padding-block: 12px; padding-left: 16px">
                   <p align="center" style="margin: 0; font-size: 14px">
                     &copy; 2024 Boltick. Todos los derechos reservados.
