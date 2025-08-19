0b1f6873a661f19838cba3b985ac274dadfd757c
Matias <martinezmatiemi@gmail.com>
2025-08-19 04:12:24 -0300

    .

--- src/app/useCases/Event/GetAllEventsUseCase/GetAllEventsUseCase.ts
+++ src/app/useCases/Event/GetAllEventsUseCase/GetAllEventsUseCase.ts
@@ -1,11 +1,35 @@
 import { IGetAllEventsUseCase, IGetAllEventsUseCaseOutput } from "./interface";
 import { IEventRepository } from "@domain/repositories/IEventRepository";
+import { EventEntity } from "@domain/entities/EventEntity";
 
 export class GetAllEventsUseCase implements IGetAllEventsUseCase {
   constructor(private eventRepository: IEventRepository) {}
 
   public async execute(): Promise<IGetAllEventsUseCaseOutput> {
     const events = await this.eventRepository.findAll();
-    return { success: 1, message: "Eventos obtenidos correctamente", data: events };
+    
+    const currentTimestamp = Date.now();
+    
+    const activeEvents: EventEntity[] = [];
+    const finishedEvents: EventEntity[] = [];
+    
+    events.forEach(event => {
+      if (event.endDate > currentTimestamp) {
+        activeEvents.push(event);
+      } else {
+        finishedEvents.push(event);
+      }
+    });
+    
+    activeEvents.sort((a, b) => a.startDate - b.startDate);
+    finishedEvents.sort((a, b) => b.startDate - a.startDate);
+    
+    const sortedEvents = [...activeEvents, ...finishedEvents];
+    
+    return { 
+      success: 1, 
+      message: "Eventos obtenidos correctamente", 
+      data: sortedEvents 
+    };
   }
 }
