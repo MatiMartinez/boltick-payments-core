0ad5f62b2f9eede74dc19da3bd237e1340572ce3
Matias <martinezmatiemi@gmail.com>
2025-08-25 20:37:22 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
-
 import { router } from "@routes/routes";
+import { corsMiddleware } from "@middlewares/CorsMiddleware";
 
 const app = express();
 
+app.use(corsMiddleware);
 app.use(express.json());
 app.use("/api", router);
 
